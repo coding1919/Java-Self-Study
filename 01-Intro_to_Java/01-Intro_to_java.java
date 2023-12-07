a56b16e4766ead798c0123e4e48e3895d92e1368
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-07 23:37:05 +0530

    What is Java and Where it is used

---
+++ 01-Intro_to_Java/01-Intro_to_java.java
@@ -0,0 +1,11 @@
+// Java is the high level programming language
+// Java is the general purpose programming language
+// Java is the object oriented programming language
+
+// Easy and used to develop any kind of programs
+
+// It is very popular because it has a very huge community through which we can get help
+
+// It can be used in Android Development
+
+// Java is a C based Language
