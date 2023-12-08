ee31bee10d9506df698a249ed7a4d9f2cdde2465
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-08 21:41:07 +0530

    What is programming,programming languages,machine language,assembly language and high level language in detail

---
+++ 02-Programming_and_programminglanguages/02-Programming_and_programminglanguages.java
@@ -0,0 +1,46 @@
+// Program:
+// It is a set of instructions that tells the computer what to do
+// Which means it is used to talk or interact with computers
+
+// Programming Languages:
+// It is used as a languages to write programs
+// Computers do not understand our(human) languages as they are a dumb machine who knows only 0 and 1
+// So this programming languages helps computer to understand 
+
+// Machine Language:
+// A computer's native language
+// Which uses 0's and 1's - Binary
+// This is hard to use
+// Theses are machine dependent - differs from different types of machines
+// Every instuction should be written in a machine language before it can be executed
+// Which means the instructions which we have written in programming languages must be translated to machine code instructions (binaries)
+
+// Assembly language:
+// It was developed to make the programming easier
+// It is machine dependent
+// It made it easier by adding the keywords
+// For ex:
+// add 2, 3, result; - This is not directly executed
+// At first a program known as ASSEMBLER translates assembly code to machine code
+
+
+// High Level Language
+// A new generation of programming languages
+// It uses english words so it makes it easy to use and learn
+// It is machine independent so the program can be executed in different machines
+// Instructions which is written is called as statement
+// The program is called as SOURCE PROGRAM or SOURCE CODE
+// For ex:
+// result = 2 + 3;
+// To translate the source code to machine code we use compiler or interpreter 
+
+// Compiler:
+// It translates all the source code to machine code
+// Source Code -> Executable
+// Source Code - Compiler -> Machine Code - Executor -> Output
+
+// Interpreter:
+// Translates each statement into machine code and executes it right away
+// Which means it executes line by line like first translates first line of code into machine code and then second line translates to machine code and so on..... 
+// Statement - Interpreter -> Output 
+
