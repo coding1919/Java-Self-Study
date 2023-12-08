defdfcb69a9785e1fd47a08e075e498abe27653f
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-08 22:50:47 +0530

    About Java, Editions of Java, API, JDK, and IDE

---
+++ 03-Details_on_Java/03-Details_on_Java.java
@@ -0,0 +1,40 @@
+// About Java:
+// It is developed by Sun Microsystems which was purchased by Oracle in 2010
+// It is a general purpose and powerful programming language
+// Used for developing software that runs on mobile,desktop and servers
+// It is machine independent
+
+// Java Language Specification
+// The syntax and semantics of Java
+// Which is like grammer in english which follows some rules same way Java follows some rules which is syntax and semantics
+// In java it is similar case like below:
+// He are playing -> Syntax Error (Grammer)
+// He is hello and bye -> Semantic Error (Meaning)
+
+// API - (Application Programming Interface):
+// It is also known as a library
+//It contains predefined Java code that we can use to develop Java programs
+// With this help it makes it faster and easier development process because we no need to write everything from scratch
+
+// Editions Of Java:
+// It has 3 editions
+// 1.Java Standard Edition (SE): develop application that run on desktop
+// 2.Java Enterprise Edition (EE): develop server-side application
+// 3.Java Micro Edition (ME): develop applications for mobile devices
+// SE is the foundation of other editions
+
+// JDK - (Java Development Kit):
+// Set of programs that enables us to develop our programs
+// Contains JRE(Java RunTime Environment) that is used to run the programs
+// Both JRE and JDK contains JVM(Java Virtual Machine)
+// JVM executes our java programs on different machines
+// Because of it Java is independent 
+
+// IDE - (Integrated Development Environment)
+// A program that allows us to write source code, compile to machine code, debug which is tools to find errors, build files that can be executed by JVM and run or execute the programs
+// This makes the development faster
+
+// Some Java IDEs
+// NetBeans
+// Eclipse
+// IntelliJ IDEA
