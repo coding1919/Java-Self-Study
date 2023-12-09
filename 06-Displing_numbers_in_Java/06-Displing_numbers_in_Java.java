fc718c134091dc82b3118ded3f818c6b600924c4
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-09 08:00:36 +0530

    How to display numbers in Java using println() method

---
+++ 06-Displing_numbers_in_Java/06-Displing_numbers_in_Java.java
@@ -0,0 +1,29 @@
+// Numbers
+// Numbers are same as in mathematics
+// Ex:
+// 0
+// -19
+// 129
+// -0.5
+// 90.81
+
+// Arithmetic Operators
+// Addition - +
+// Subtraction - -
+// Multiplication - *
+// Division - /
+
+// Printing
+System.out.println(4);
+System.out.println(5 + 2);
+System.out.println(3 - 5);
+System.out.println(6 * 3);
+System.out.println(4 / 2);
+// output:
+// 4
+// 7
+// -2
+// 18
+// 2
+
+// The value will be calculated and then result will be printed
