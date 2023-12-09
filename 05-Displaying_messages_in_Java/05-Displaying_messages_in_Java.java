eb79b948a13c479af06ae16a66b9f60f9025b3e7
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-09 07:50:15 +0530

    How to display messages in Java using print() and println() methods and also info on strings

--- 05-Displaying_messages_in_Java/05-Displaying_messages_in_Java.java
+++ 05-Displaying_messages_in_Java/05-Displaying_messages_in_Java.java
@@ -11,8 +11,34 @@
 
 // Calling println()
 // display its parameter on the console window
-System.out.println("hello");
+System.out.println("hello"); //statement
 System.out.println("123");
 System.out.println("");
 System.out.println("456");
 // println in which ln means to print in new line
+// output:
+// hello
+// 123
+// 
+// 456
+
+
+// Calling print()
+// display its parameter on the console window
+System.out.println("hello"); //statement
+System.out.println("123");
+System.out.println("");
+System.out.println("456");
+// output:
+// hello123456
+
+// System.out:
+// 'out' is an object of the 'PrintStream'Class
+// 'out' has the print() and println() methods
+// we use '.' to access print()/println() of out.
+// 'out' refers to the standard output device (Screen)
+// 'System' is a class (Pascal case convention)
+// 'out' is inside System (field)
+// we use '.' to access out of System
+System.out.println()
+System.out.print()
