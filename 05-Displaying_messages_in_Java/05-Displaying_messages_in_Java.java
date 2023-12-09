84e784e7749282441b64def669b2474fa578f04e
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-09 07:48:37 +0530

    How to display messages in Java using print() and println() methods and also info on strings

---
+++ 05-Displaying_messages_in_Java/05-Displaying_messages_in_Java.java
@@ -0,0 +1,18 @@
+// Strings:
+// Group of characters(Text)
+// Strings in Java should be put inside the double quotes
+// Ex: 
+// "hello"
+// "this is a string"
+// "1"
+// "125"
+// "!@123Ad"
+// ""
+
+// Calling println()
+// display its parameter on the console window
+System.out.println("hello");
+System.out.println("123");
+System.out.println("");
+System.out.println("456");
+// println in which ln means to print in new line
