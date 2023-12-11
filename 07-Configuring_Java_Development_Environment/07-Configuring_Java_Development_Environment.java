a857eac4c717318414c9860ae634f298e217a546
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-11 23:41:45 +0530

    Installing JDK and Intellij idea

---
+++ 07-Configuring_Java_Development_Environment/07-Configuring_Java_Development_Environment.java
@@ -0,0 +1,39 @@
+//  Download Intellij IDEA by searching through the google
+// https://www.jetbrains.com/idea/download/?section=windows
+// Download the community version which is free and open source
+// After that download JDK by searching jdk download
+// https://www.oracle.com/java/technologies/downloads/
+// Choose the appropriate OS and install JDK
+
+// Installing JDK
+// Press next
+// Press next
+// after loading fully
+// close
+
+// Setting Environment Variables
+// This helps the computer to find where is JDK
+// This PC right click and go to properties
+// Press advanced system settings
+// Then press environment variables
+// Under system variables press new
+// variable name - JAVA_HOME
+// variable value - give the path
+// for ex like this - C:\Program Files\Java\jdk-21
+// in there after pressing ok search there itself path and click edit 
+// new and type %JAVA_HOME%\bin\
+// then press ok for all
+// go for command prompt
+// and type java -version
+// for ex - 
+// C:\Users\safwa>java -version
+// java version "21.0.1" 2023-10-17 LTS
+// Java(TM) SE Runtime Environment (build 21.0.1+12-LTS-29)
+// Java HotSpot(TM) 64-Bit Server VM (build 21.0.1+12-LTS-29, mixed mode, sharing)
+
+// Installing Intellij Idea 
+// Press next 
+// Press next 
+// Press next 
+// Press install
+// Then use it
