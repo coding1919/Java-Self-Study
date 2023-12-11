dbd87c2b0496e1d60ec9f4a4e42934cf9863ea81
coding1919 <xgchuvuvughcgchchc@gmail.com>
2023-12-11 23:52:17 +0530

    create compile and execute the first java program using notepad and terminal

--- 08-Creating_Compiling_&_Executing_a_Java_Program/08-Creating_Compiling_&_Executing_a_Java_Program.java
+++ 08-Creating_Compiling_&_Executing_a_Java_Program/08-Creating_Compiling_&_Executing_a_Java_Program.java
@@ -1,6 +1,19 @@
-public class Main{
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-	}
-}
+// Open a notepad and write this code
+
+// public class Main{
+// 	public static void main(String[] args){
+// 		System.out.println("Hello World!");
+// 	}
+// }
 	
+// Then save it as the class_name.java like in my case the class name is Main so i will save it as Main.java
+// Then in save as type select all files and save it
+// Then go to command prompt and access your particular java file through command prompt like if it is saved in Desktop then cd Desktop or another method is right click the particular folder in which the java file is saved and the click on open in terminal
+// then type javac Main.java
+// and click enter and also you will see that new Main.class file is created to see that file in command prompt you can type like this dir Main.*
+// Now to run it after successfully Main.class file is created that is in your case it will be class_name.class file be created and then type this to execute the code
+// java Main or in yout case it will be java class_name
+// and we will see Hello World! is printed
+// java byte code file executed by JVM can run on any computer
+// ********************Congragulations********************
+
